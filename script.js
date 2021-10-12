84b4b9653a32c5a3cf10c92c74140e81d22beaba
AlexeyDenysiuk <alexey11denisuk18@gmail.com>
2021-10-12 13:13:11 +0300

    1 task + some rubbish

---
+++ script.js
@@ -0,0 +1,4 @@
+//Task 1
+let temp = document.getElementById("block_1").innerHTML;
+document.getElementById("block_1").innerHTML = document.getElementById("block_6").innerHTML;
+document.getElementById("block_6").innerHTML = temp;
